0b7208b5cf86fdb99716959ce94e5288f2a93382
sanjana <sanjanakiei20@gmail.com>
2024-09-06 12:12:44 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,7 +13,6 @@ export default function Header() {
            <Link to='/about'> <li>About</li></Link>
            <Link to='/contact'> <li>Contact</li></Link>
            <Link to='/sign-in'> <li>Sign In</li></Link>
-           <Link to='/sign-up'> <li>Sign Up</li></Link>
 
         </ul>
         </div>
